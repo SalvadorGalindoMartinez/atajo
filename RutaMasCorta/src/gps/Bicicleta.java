c84f3d4a6d6274c933a8614077a7a4a65112f626
Salvador Galindo Martínez <salva150809@gmail.com>
2019-02-11 18:11:34 +0100

    SolucionadoFinal

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -13,44 +13,6 @@ public class Bicicleta {
         String marca = "Orbea";
     }
     
-    /*public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
-    {
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        
-        for(int i=0; i<camino1.length; i++)
-        {
-            sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
-        }
-        
-        if(sum1 < sum2 && sum1 < sum3)
-        {
-            return "camino 1"; 
-        }else if(sum2 < sum3 && sum2 < sum1)
-        {
-            return "camino 2";
-        }else if(sum3 < sum1 && sum3 < sum2)
-        {
-            return "camino 3";
-        }else{
-            if(sum1 == sum2 && sum2 == sum3)
-            {
-                return "camino 1, 2 y 3";
-            }else if(sum1 == sum3)
-            {
-                return "caminos 1 y 3";
-            }else if(sum2 == sum3){
-                return "caminos 2 y 3";
-            }else{
-                return "caminos 1 y 2";
-            }
-        }
-        
-    }*/
-    
     public static String rutaCorta(int [] camino1, int [] camino2, int []camino3) {
     	int sum1=0;
     	int sum2=0;
