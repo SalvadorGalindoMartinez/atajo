add3f87ce6c16c7f5a40b84455adee9c1a051cab
Salvador Galindo Martínez <salva150809@gmail.com>
2019-02-11 18:10:24 +0100

    Solucionado

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -13,7 +13,7 @@ public class Bicicleta {
         String marca = "Orbea";
     }
     
-    public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
+    /*public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
     {
         int sum1 = 0;
         int sum2 = 0;
@@ -49,6 +49,49 @@ public class Bicicleta {
             }
         }
         
+    }*/
+    
+    public static String rutaCorta(int [] camino1, int [] camino2, int []camino3) {
+    	int sum1=0;
+    	int sum2=0;
+    	int sum3=0;
+    	
+    	for(int i =0; i<camino1.length; i++) {
+    		sum1 += camino1[i];
+    	}
+    	// He añadido estos dos for para que sume correctamente los otro caminos que antes no estaba sumando en su totalidad
+    	for(int i=0; i<camino2.length; i++) {
+    		sum2 += camino2[i];
+    	}
+    	
+    	for(int i=0; i<camino3.length; i++) {
+    		sum3 += camino3[i];
+    	}
+    	// He añadido esta línea también para comprobar que hace las sumas de los caminos correctamente
+    	System.out.println("camino1: " + sum1 + ", camino2: " + sum2 + ", camino3: " + sum3);
+    	
+    	if(sum1 < sum2 && sum1 < sum3)
+        {
+            return "camino 1"; 
+        }else if(sum2 < sum3 && sum2 < sum1)
+        {
+            return "camino 2";
+        }else if(sum3 < sum1 && sum3 < sum2)
+        {
+            return "camino 3";
+        }else{
+            if(sum1 == sum2 && sum2 == sum3)
+            {
+                return "camino 1, 2 y 3";
+            }else if(sum1 == sum3)
+            {
+                return "caminos 1 y 3";
+            }else if(sum2 == sum3){
+                return "caminos 2 y 3";
+            }else{
+                return "caminos 1 y 2";
+            }
+        }
     }
 
     public static void main(String[] args) {
